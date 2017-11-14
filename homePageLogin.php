3fdeb46bae9037934c529cb8bb0765bf3e4d7d12
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-14 18:57:40 -0500

    new homepage

---
+++ homePageLogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+    <div class="">
+      <a href="#">View Yardsales</a>
+      <a href='/yardSale/newYardSalePage.php'>Create Yardsale</a>
+      <a href='/yardSale/loginPage.php'>Login</a>
+      <a href='/yardSale/registerPage.php'>Register</a>
+    </div>
+
+    <hr>
+
+    <div class="">
+      <?php  ?>
+    </div>
+
+
+  </body>
+
+</html>
