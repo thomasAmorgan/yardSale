efb48ff718f75ab4af0650770ff70416cdee7d8a
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-14 19:02:45 -0500

    added another homepage

--- homePageLogin.php
+++ homePageLogin.php
@@ -10,8 +10,7 @@
     <div class="">
       <a href="#">View Yardsales</a>
       <a href='/yardSale/newYardSalePage.php'>Create Yardsale</a>
-      <a href='/yardSale/loginPage.php'>Login</a>
-      <a href='/yardSale/registerPage.php'>Register</a>
+      <a href='/yardSale/loginPage.php'>Logout</a>
     </div>
 
     <hr>
