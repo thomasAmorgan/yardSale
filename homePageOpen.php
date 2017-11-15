57dd3ace98a689a9b6cbadebc9415a0b92d7714d
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-14 19:04:37 -0500

    actually added new homepage

---
+++ homePageOpen.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+    <div class="">
+      <a href="#">View Yardsales</a>
+      <a href='/yardSale/loginPage.php'>Login</a>
+      <a href='/yardSale/registerPage.php'>Register</a>
+    </div>
+
+    <hr>
+
+    <div class="">
+      <?php  ?>
+    </div>
+
+
+  </body>
+
+</html>
