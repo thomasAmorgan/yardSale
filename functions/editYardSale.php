9409249cf42278a082a5f289b30a5644097844c6
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-20 21:58:43 -0500

    trying to get editYardSale to display the editing form

--- functions/editYardSale.php
+++ functions/editYardSale.php
@@ -14,6 +14,17 @@
 
       $userName = $_SESSION['userName'];
       $yardSaleID = $_POST['editYardSale'];
+
+      $userID;
+      $yardSaleDate;
+      $yardSaleTime;
+      $streetAddress;
+      $yardSaleName;
+      $yardSaleDescription;
+      $state;
+      $zipCode;
+      $city;
+      $
     ?>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~ END: CHECK IF LOGGED IN ~~~~~~~~~~~~~~~~~~~~~~~ -->
   </head>
@@ -29,7 +40,75 @@
     </div>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END: NAVBAR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 
+    <hr>
 
+    <div class="">
+      <?php
+        if (!empty($_POST['editYardSale'])) {
+          $findYardSaleMatch = "SELECT * FROM YardSales
+                                WHERE yardSaleID = '$yardSaleID'";
+
+          $matchResult = $mysqli->query($findYardSaleMatch);
+
+          if ($matchResult->num_rows > 0) {
+            while ($row = $matchResult->fetch_assoc()) {
+              $userID = $row['userID'];
+              $yardSaleDate = $row['yardSaleDate'];
+              $yardSaleTime = $row['yardSaleTime'];
+              $streetAddress = $row['streetAddress'];
+              $yardSaleName = $row['yardSaleName'];
+              $yardSaleDescription = $row['yardSaleDescription'];
+              $state = $row['state'];
+              $zipCode = $row['zipCode'];
+              $city = $row['city'];
+            }
+          }
+
+          echo "	<div>
+          		<form action='' method='post'>
+                <p><b>Yardsale Name</b></p>
+          			<label for='yardSaleName'>Name: </label>
+          			<input type='text' name='yardSaleName' id='yardSaleName' required>
+                <br>
+
+                <p><b>Yardsale Address</b></p>
+          			<label for='yardSaleStreet'>Street: </label>
+          			<input type='text' name='yardSaleStreet' id='yardSaleStreet' required>
+                <br>
+          			<label for='yardSaleCity'>City: </label>
+          			<input type='text' name='yardSaleCity' id='yardSaleCity' required>
+          			<label for='yardSaleState'>State: </label>
+          			<select id='states' name='yardSaleState'> </select>
+                <br>
+                <label for='yardSaleZip'>Zip Code: </label>
+          			<input type='text' name='yardSaleZip' id='yardSaleZip' required>
+          			<br>
+
+                <p><b>Date of Yardsale</b></p>
+          			<label for='months'>Month: </label>
+          			<select id='months' name='yardSaleMonth'> </select>
+          			<label for='days'>Day: </label>
+          			<select id='days' name='yardSaleDay'> </select>
+          			<label for='years'>Year: </label>
+          			<select id='years' name='yardSaleYear'> </select>
+                <br>
+                <label for='yardSaleHour'>Time: </label>
+          			<select id='hours' name='yardSaleHour'> </select>
+          			<select id='ampm' name='yardSaleAMPM'> </select>
+
+                <br>
+          			<!-- <label for='description'>Description: </label> -->
+                <p><b>Description</b></p>
+          			<textarea id='description' name='yardSaleDescription' rows='10' cols='50' required></textarea>
+
+          			<br>
+          			<br>
+          			<button type='submit' formmethod='post' name='button'>Create</button>
+          		</form>
+          	</div>";
+        }
+       ?>
+    </div>
 
 
   </body>
