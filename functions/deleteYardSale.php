8dcb93e4a53f970c76fda6b472909e2ff9599ddb
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-22 14:43:03 -0500

    if you delete a yardsale it deletes its items too

--- functions/deleteYardSale.php
+++ functions/deleteYardSale.php
@@ -12,10 +12,20 @@
 
   // also needs to delete items associated with the yardSale
   if (!empty($_POST)) {
-    $deleteQuery = "DELETE FROM YardSales WHERE yardSaleID = '$yardSaleID'
+    $deleteQuery = "DELETE FROM YardSales
+                    WHERE yardSaleID = '$yardSaleID'
                     AND userID = '$userID'";
 
     if ($mysqli->query($deleteQuery) === true) {
+
+      $deleteItems = "DELETE FROM Merchandise
+                      WHERE yardSaleID = '$yardSaleID'
+                      AND userID = '$userID'"
+
+      if ($mysqli->query($deleteItems) === true) {
+        header("Location: /yardSale/userPage.php");
+      }
+
       header("Location: /yardSale/userPage.php");
     }
 
