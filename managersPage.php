7146aa773cb078425038ac79766d6eda61adeffe
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-22 15:06:43 -0500

    if you're not a manager you can't access the managers page

--- managersPage.php
+++ managersPage.php
@@ -16,7 +16,7 @@
       $isManager = false;
 
       $checkManager = "SELECT * FROM UserProfiles
-                       WHERE manager = 'true'
+                       WHERE manager = true
                        AND userID = '$userID'";
 
       $checkManagerResult = $mysqli->query($checkManager);
