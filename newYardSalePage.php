3facbcadc17a5361beef512470b01abf547c44e9
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-24 12:36:50 -0500

    trying to insert adprice and currentpromotion into a yardsale

--- newYardSalePage.php
+++ newYardSalePage.php
@@ -69,23 +69,23 @@
       $yardSaleID = "$userID" . "$randNum";
       $_SESSION['yardSaleID'] = $yardSaleID;
 
-      $createYardSaleQuery = "INSERT INTO YardSales (yardSaleID, userID,
-                              yardSaleDate, yardSaleTime, streetAddress,
-                              yardSaleName, yardSaleDescription, state,
-                              zipCode, city, discountPercentage, adPrice)
-                              VALUES ('$yardSaleID', '$userID', '$yardSaleDate',
-                              '$yardSaleTime', '$yardSaleStreet', '$yardSaleName',
-                              '$yardSaleDescription', '$yardSaleState',
-                              '$yardSaleZip', '$yardSaleCity', $currentPromotion
-                              $adPrice)";
       // $createYardSaleQuery = "INSERT INTO YardSales (yardSaleID, userID,
       //                         yardSaleDate, yardSaleTime, streetAddress,
       //                         yardSaleName, yardSaleDescription, state,
-      //                         zipCode, city)
+      //                         zipCode, city, discountPercentage, adPrice)
       //                         VALUES ('$yardSaleID', '$userID', '$yardSaleDate',
       //                         '$yardSaleTime', '$yardSaleStreet', '$yardSaleName',
       //                         '$yardSaleDescription', '$yardSaleState',
-      //                         '$yardSaleZip', '$yardSaleCity')";
+      //                         '$yardSaleZip', '$yardSaleCity', $currentPromotion
+      //                         $adPrice)";
+      $createYardSaleQuery = "INSERT INTO YardSales (yardSaleID, userID,
+                              yardSaleDate, yardSaleTime, streetAddress,
+                              yardSaleName, yardSaleDescription, state,
+                              zipCode, city)
+                              VALUES ('$yardSaleID', '$userID', '$yardSaleDate',
+                              '$yardSaleTime', '$yardSaleStreet', '$yardSaleName',
+                              '$yardSaleDescription', '$yardSaleState',
+                              '$yardSaleZip', '$yardSaleCity')";
 
       $createYardSaleResult = $mysqli->query($createYardSaleQuery);
 
