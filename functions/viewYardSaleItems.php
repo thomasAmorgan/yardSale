9e9cc361132d9ceb2db31e776144be575aa342ad
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-27 15:58:09 -0500

    displaying all yardsales with profits in managers page

--- functions/viewYardSaleItems.php
+++ functions/viewYardSaleItems.php
@@ -61,7 +61,8 @@
                  while ($row = $displayResult->fetch_assoc()) {
                    echo "<br><b> Merch ID: " . $row["merchID"] . "</b> <br>" .
                         "Name: " . $row["itemName"] . "<br>" .
-                        "Price: $" . $row["price"] .  "<br>" .
+                        "Price: $" . $row["price"] .  "<br>";
+                        
                    if ($row['sold'] == 0) {
                      echo "Sold: Not Sold <br>";
                    }
