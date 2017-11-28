71afabfd9434ec9be715422dacf139777b028e36
thomasAmorgan <thomasmorgan233@gmail.com>
2017-11-27 23:37:54 -0500

    adding a filter for to look at various income statistics in manager

--- managersPage.php
+++ managersPage.php
@@ -35,6 +35,38 @@
 
     <hr>
 
+<!-- ~~~~~~~~~~~~~~~~~ START: CHANGE PROMOS/PRICE FORM ~~~~~~~~~~~~~~~~~~~~~ -->
+    <div class="">
+      <form class="" action="functions/updatePromoPrice.php" method="post">
+        <p>Adjust the promotion percent or the amount per advertisement.</p>
+        <p>Promotion should be entered in as a decimal value example 0.4 = 40%</p>
+        <input type="number" step="0.01" name="amount" id="amount">
+        <select name="promoPrice" id="promoPrice">
+          <option value="currentPromotion">Promotion</option>
+          <option value="adPrice">Price</option>
+        </select>
+        <button type="submit" formmethod="post">Update</button>
+      </form>
+    </div>
+<!-- ~~~~~~~~~~~~~~~~~~ END: CHANGE PROMOS/PRICE FORM ~~~~~~~~~~~~~~~~~~~~~~ -->
+
+    <div class="">
+      <p><b>Current Promotion and Price</b></p>
+        <?php
+          $currentPromoPrice = "SELECT * FROM Discount";
+
+          $result = $mysqli->query($currentPromoPrice);
+
+          if ($result->num_rows > 0) {
+
+            while ($row = $result->fetch_assoc()) {
+              echo "Promotion: " . (($row["currentPromotion"] * 100)) . "%<br>" .
+                   "Advertisement Price: $" . $row["adPrice"] .  "<br>";
+            }
+          }
+          ?>
+    </div>
+
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~ START: SEARCH FORM ~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
     <div class="">
       <p>To filter statistics for last week, month, or year select an option below and click filter</p>
@@ -66,6 +98,8 @@
         $currentDay = substr($currentDate, 3, 2);
         $currentYear = substr($currentDate, 6, 4);
 
+        $lastTotal = 0;
+
         echo "<br>SEPERATED - MONTH: " . $currentMonth . " DAY: " . $currentDay . " YEAR: " . $currentYear;
 
         if ($searchResult->num_rows > 0) {
@@ -79,13 +113,34 @@
             $yardSaleDay = substr($yardSaleDate, 3, 2);
             $yardSaleYear = substr($yardSaleDate, 6, 4);
 
-            if ($_POST['incomeStatistic'] == week) {
+            $lastPromo = $row['discountPercentage'];
+            $lastPrice = $row['adPrice'];
+
+            $lastCalculatedPrice = $lastPrice - ($lastPromo * $lastPrice);
+
+            if ($_POST['incomeStatistic'] == "week") {
               echo "<br>WEEK";
             }
 
-            elseif ($_POST['incomeStatistic'] == month) {
+            elseif ($_POST['incomeStatistic'] == "month") {
               echo "<br>MONTH: ";
-              echo (int)$yardSaleMonth;
+              if ((int)$yardSaleMonth - 1 < (int)$currentMonth) {
+                //figure out how to check for january
+
+                $lastTotal += $calculatedPrice;
+
+                echo "<b>Name: " . $row["yardSaleName"] . "</b><br>" .
+                "<b> Yardsale ID: " . $row["yardSaleID"] . "</b> <br>" .
+                "Host: " . $row["userID"] . "<br>" .
+                "Date: " . $row["yardSaleDate"] . "<br>" .
+                "Promotion: " . ($row["discountPercentage"] * 100) . "%<br>" .
+                "Price: $" . $row["adPrice"] . "<br>" .
+                "Profit: $" . ($row['adPrice'] - ($row["discountPercentage"] * $row['adPrice'])) . "<br><br>";
+              }
+
+              else {
+                echo "<br>There were no yardsales last month.";
+              }
             }
 
             else {
@@ -110,59 +165,29 @@
   </div>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~ START: SEARCH FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 
-<!-- ~~~~~~~~~~~~~~~~~ START: CHANGE PROMOS/PRICE FORM ~~~~~~~~~~~~~~~~~~~~~ -->
-    <div class="">
-      <form class="" action="functions/updatePromoPrice.php" method="post">
-        <p>Adjust the promotion percent or the amount per advertisement.</p>
-        <p>Promotion should be entered in as a decimal value example 0.4 = 40%</p>
-        <input type="number" step="0.01" name="amount" id="amount">
-        <select name="promoPrice" id="promoPrice">
-          <option value="currentPromotion">Promotion</option>
-          <option value="adPrice">Price</option>
-        </select>
-        <button type="submit" formmethod="post">Update</button>
-      </form>
-    </div>
-<!-- ~~~~~~~~~~~~~~~~~~ END: CHANGE PROMOS/PRICE FORM ~~~~~~~~~~~~~~~~~~~~~~ -->
-
-    <div class="">
-      <p><b>Current Promotion and Price</b></p>
-      <?php
-        $currentPromoPrice = "SELECT * FROM Discount";
-
-        $result = $mysqli->query($currentPromoPrice);
-
-        if ($result->num_rows > 0) {
-
-          while ($row = $result->fetch_assoc()) {
-            echo "Promotion: " . (($row["currentPromotion"] * 100)) . "%<br>" .
-                 "Advertisement Price: $" . $row["adPrice"] .  "<br>";
-          }
-        }
-       ?>
-    </div>
-
 <!-- ~~~~~~~~~~~~~~~~~~~~ START: DISPLAY TOTAL PROFITS ~~~~~~~~~~~~~~~~~~~~~ -->
     <div class="">
-      <p><b>Total Income</b></p>
       <?php
-        $income = 0;
-        $totalIncomeQuery = "SELECT discountPercentage, adPrice FROM YardSales";
+        if (empty($_POST['incomeStatistic'])) {
+          echo "<p><b>Total Income</b></p>";
+          $income = 0;
+          $totalIncomeQuery = "SELECT discountPercentage, adPrice FROM YardSales";
 
-        $incomeResult = $mysqli->query($totalIncomeQuery);
+          $incomeResult = $mysqli->query($totalIncomeQuery);
 
-        if ($incomeResult->num_rows > 0) {
-          while ($row = $incomeResult->fetch_assoc()) {
-            $promo = $row['discountPercentage'];
-            $price = $row['adPrice'];
+          if ($incomeResult->num_rows > 0) {
+            while ($row = $incomeResult->fetch_assoc()) {
+              $promo = $row['discountPercentage'];
+              $price = $row['adPrice'];
 
-            $calculatedPrice = $price - ($promo * $price);
+              $calculatedPrice = $price - ($promo * $price);
 
-            $income += $calculatedPrice;
+              $income += $calculatedPrice;
 
 
+            }
+            echo "Income: $" . $income;
           }
-          echo "Income: $" . $income;
         }
       ?>
     </div>
@@ -171,9 +196,9 @@
 <!-- ~~~~~~~~~~~~~~~~~~~~ START: DISPLAY YS FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~ -->
 <!-- Displays all yardsales in the db -->
     <div class="">
-      <br>
-      <p><b>Yardsale Incomes</b></p>
       <?php
+        if (empty($_POST['incomeStatistic'])) {
+          echo "<br><p><b>Yardsale Incomes</b></p>";
           $allYardSales = "SELECT * FROM YardSales";
           $result = $mysqli->query($allYardSales);
 
@@ -192,7 +217,8 @@
           else {
             echo "<br> <br>There are no yardsales";
           }
-       ?>
+        }
+      ?>
     </div>
 <!-- ~~~~~~~~~~~~~~~~~~~~~~ END: DISPLAY YS FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~ -->
 
